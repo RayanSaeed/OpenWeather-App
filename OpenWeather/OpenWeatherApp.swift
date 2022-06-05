c2b0636f5b0d028a8315e08766f18b9e52a21e41
Rayan Saeed <rayancodes@gmail.com>
2022-06-05 20:33:36 +0500

    Initial commit

---
+++ OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Rayan Saeed on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
